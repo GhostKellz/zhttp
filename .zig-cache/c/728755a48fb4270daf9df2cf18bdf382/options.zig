5da79c595d3b83244a8202e6c624a35681776070
GhostKellz <ckelley@ghostkellz.sh>
2025-10-04 23:51:10 -0400

    full cleanup - native async, http1/2/3 implementation, modular build system

--- .zig-cache/c/728755a48fb4270daf9df2cf18bdf382/options.zig
+++
@@ -1,7 +0,0 @@
-pub const engine_h1: bool = true;
-pub const engine_h2: bool = false;
-pub const engine_h3: bool = false;
-pub const enable_async: bool = true;
-pub const with_brotli: bool = false;
-pub const with_zlib: bool = true;
-pub const quic_backend: []const u8 = "none";
